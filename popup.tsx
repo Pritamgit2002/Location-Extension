e905428bbf771be430ce9c615bfd1010a22e74ed
Pritam Gain <pritamjob2021@gmail.com>
2023-08-31 20:34:57 +0530

    Final_Update

--- popup.tsx
+++ popup.tsx
@@ -1,3 +1,7 @@
+
+
+
+
 import axios from "axios"
 import { useEffect, useState } from "react"
 //import dotenv from "dotenv";
@@ -6,59 +10,57 @@ import "./style.css"
 
 function IndexPopup() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [data, setData] = useState<string | null>(null)
   const [country, setCountry] = useState<string | null>(null)
   const [city, setCity] = useState<string | null>(null)
   const [error, setError] = useState<string | null>(null)
 
-  useEffect(() => {
-    if (data) {
-     // const apiKey = "afed51115c09d7"
-     //console.log(PLASMO_PUBLIC_IPINFO_TOKEN);
-     const apiKey = process.env.PLASMO_PUBLIC_IPINFO_TOKEN;
-     
-     console.log(apiKey);
-      axios
-        .get(`http://ipinfo.io/${data}/json?token=${apiKey}`)
-        .then((res) => {
-          const ipCountry = res.data.country
-          const ipCity = res.data.city
+  const fetchData=async()=>{
+      try {
+          const apiKey = process.env.PLASMO_PUBLIC_IPINFO_TOKEN;
+          setCountry(null)
+          setCity(null)
+          setIsLoading(true)
+          const res =await axios.get("https://api.ipify.org?format=json")
+          const ip= res.data.ip;
+          console.log(ip);
+          const res_location=await axios.get(`http://ipinfo.io/${ip}/json?token=${apiKey}`)
+          console.log(res_location);
+          const ipCountry = res_location.data.country
+          const ipCity = res_location.data.city
           setCountry("Your country is " + ipCountry)
-          setCity(" and your city is " + ipCity + ".")
-        })
-        .catch((err) => {
+          setCity("Your city is " + ipCity )
+          
+      } catch (error) {
           setError("Something went Wrong!!! Try Again ...")
-        })
-    }
-  }, [data])
-
-  const handleClick = () => {
-      setIsLoading(true)
-
-    fetch("https://api.ipify.org?format=json")
-      .then((res) => res.json())
-      .then((ipUser) => {
-        // setTimeout(() => {
-          setData(ipUser.ip)
-          setIsLoading(false)
-        // }, )
-      })
+      }
+      finally{
+        setIsLoading(false);
+      }
+    
   }
 
-  return (
-    <div className="bg-sky-400 w-[400px] h-[400px] flex flex-col items-center justify-center">
-      <h2 className="text-2xl font-sans font-bold text-center py-5 text-white">
-        <span>{country}</span>
-        <br />
-        {city}
-      </h2>
+ 
 
-      <button
-        onClick={handleClick}
+  
+
+  return (
+    <div className="bg-blue-200 w-[400px] h-[400px] flex flex-col items-center justify-center border-4 border-double border-blue-600 ">
+      {country&&city && <div className="text-2xl font-sans flex flex-col gap-3 font-bold text-center mb-5 text-black uppercase">
+        <div>{country}</div>
+        
+        <div>{city}</div>
+      </div>}
+
+    {isLoading && 
+      <div>
+        <img src="https://cdn.discordapp.com/attachments/938278149622472714/1146801792508756049/18-location-pin.png" alt="logo" height={200} width={180} className="animate-bounce"/>
+      </div>}
+      {!isLoading&&<button
+        onClick={fetchData}
         disabled={isLoading}
-        className="w-fit bg-slate-500 p-5 tracking-wider rounded-lg text-[24px] font-semibold shadow-lg shadow-slate-400">
-        {isLoading ? "Loading..." : "Press to see your location"}
-      </button>
+        className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-xl px-5 py-3 text-center ">
+        Click to see your location
+      </button>}
 
       {error && (
         <div className="bg-red-500 text-white m-2 text-md p-1 rounded-lg font-semibold">
